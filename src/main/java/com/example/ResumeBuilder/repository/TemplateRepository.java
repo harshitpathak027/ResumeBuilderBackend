142f9d9cfafedae23397b85582dfaa007193c266
Harshit Pathak <harshitpathak@192.168.1.4>
2026-06-21 22:22:21 +0530

    update

--- src/main/java/com/example/ResumeBuilder/repository/TemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ResumeBuilder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemplateRepository extends JpaRepository<com.example.ResumeBuilder.model.Template, Long>{
-    
-}
